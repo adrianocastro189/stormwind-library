8ea3765670b34ac574b0bc336f3ede2fa3638622
Adriano Castro <adrianostanley@gmail.com>
2024-02-22 10:55:35 -0300

    Update the site title

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Stormwind Library',
+  tagline: 'A library developed in Lua to serve as a framework for building World of Warcraft addons.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Stormwind Library',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Stormwind Library',
           src: 'img/logo.svg',
         },
         items: [
