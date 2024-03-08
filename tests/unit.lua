5de03bb99b32489a72a4f6b03b651e0721315fcd
Adriano Castro <adrianostanley@gmail.com>
2024-03-08 14:21:06 -0300

    Add a test file for the Target facade

--- tests/unit.lua
+++ tests/unit.lua
@@ -3,6 +3,8 @@ lu = require('luaunit')
 dofile('./dist/stormwind-library.lua')
 __ = StormwindLibrary_v0_0_4.new()
 
+dofile('./tests/Facades/TargetTest.lua')
+
 dofile('./tests/Support/ArrTest.lua')
 
 os.exit(lu.LuaUnit.run())
