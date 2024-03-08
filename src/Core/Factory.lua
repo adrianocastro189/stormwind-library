056b20ec8e915cafc75023fee802f74aa065922a
Adriano Castro <adrianostanley@gmail.com>
2024-03-08 16:53:09 -0300

    Add the Factory:addClass() method

--- src/Core/Factory.lua
+++ src/Core/Factory.lua
@@ -4,6 +4,15 @@ Contains a list of classes that can be instantiated by the library.
 self.classes = {}
 
 --[[
+Registers a class so the library is able to instantiate it later.
+
+This method's the same as updating self.classes.
+]]
+function self:addClass(classname, classStructure)
+    self.classes[classname] = classStructure
+end
+
+--[[
 This method emulates the new keyword in OOP languages by instantiating a
 class by its name as long as the class has a __construct() method with or
 without parameters.
---
+++ tests/Core/FactoryTest.lua
@@ -0,0 +1,25 @@
+--[[
+@covers Factory.classes
+@covers Factory:addClass()
+@covers Factory:new()
+]]
+function testFactoryCanInstantiateClasses()
+    local MockClass = {}
+    MockClass.__index = MockClass
+
+    function MockClass.__construct(name)
+        local self = setmetatable({}, MockClass)
+        self.name = name
+        return self
+    end
+
+    local library = newLibrary()
+    library:addClass('MockClass', MockClass)
+
+    lu.assertNotIsNil(library.classes)
+
+    local mockClassInstance = library:new('MockClass', 'test-name')
+
+    lu.assertNotIsNil(mockClassInstance)
+    lu.assertEquals(mockClassInstance.name, 'test-name')
+end
