1dc7390082c04c54861ab9396b8b259f0f75fa91
Adriano Castro <adrianostanley@gmail.com>
2024-03-16 14:35:05 -0300

    Add the Command chained methods

--- src/Commands/Command.lua
+++ src/Commands/Command.lua
@@ -1,6 +1,15 @@
 --[[
 The command class represents a command in game that can be executed with
-/commandName
+/commandName.
+
+Commands in the Stormwind Library are structured in three parts being:
+
+1. The command name
+2. The command action
+3. The command arguments
+
+That said, a command called myAddonCommand that shows its settings screen
+in dark mode would be executed with /myAddonCommand show darkMode.
 ]]
 local Command = {}
     Command.__index = Command
@@ -12,5 +21,35 @@ local Command = {}
     ]]
     function Command.__construct()
         return setmetatable({}, Command)
+    end
+
+    --[[
+    Sets the command action.
+
+    @return self
+    ]]
+    function Command:setAction(action)
+        self.action = action
+        return self
+    end
+
+    --[[
+    Sets the command callback.
+
+    @return self
+    ]]
+    function Command:setCallback(callback)
+        self.callback = callback
+        return self
+    end
+    
+    --[[
+    Sets the command name.
+
+    @return self
+    ]]
+    function Command:setName(name)
+        self.name = name
+        return self
     end
 -- end of Command
--- tests/Commands/CommandsTest.lua
+++ tests/Commands/CommandsTest.lua
@@ -1,4 +1,20 @@
 TestCommand = {}
+    -- @covers Command:setAction()    
+    -- @covers Command:setCallback()
+    -- @covers Command:setName()
+    function TestCommand:testChainedSetters()
+        local command = __:new('Command')
+        
+        command
+            :setName('test-name')
+            :setAction('test-action')
+            :setCallback('test-callback')
+
+        lu.assertEquals('test-name', command.name)
+        lu.assertEquals('test-action', command.action)
+        lu.assertEquals('test-callback', command.callback)
+    end
+
     -- @covers Command:__construct()
     function TestCommand:testInstantiate()
         local command = __:new('Command')
