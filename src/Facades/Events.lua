ddc69dd57797b6ba716570ac8b3ae857be9c2dd9
Adriano Castro <adrianostanley@gmail.com>
2024-03-26 20:35:12 -0300

    Add the Events:handleOriginal() method

--- src/Facades/Events.lua
+++ src/Facades/Events.lua
@@ -1,5 +1,9 @@
 --[[
-Class description.
+The Events class is a layer between World of Warcraft events and events
+triggered by the Stormwind Library.
+
+When using this library in an addon, it should focus on listening to the
+library events, which are more detailed and have more mapped parameters.
 ]]
 local Events = {}
     Events.__index = Events
@@ -12,7 +16,26 @@ local Events = {}
     function Events.__construct()
         local self = setmetatable({}, Events)
 
+        -- the list of library listeners to World of Warcraft events
+        self.originalListeners = {}
+
         return self
+    end
+
+    --[[
+    This is the main event handler method, which will capture all
+    subscribed World of Warcraft events and forwards them to the library
+    handlers that will later notify other subscribers.
+
+    It's important to mention that addons shouldn't care about this
+    method, which is an internal method to the Events class.
+    ]]
+    function Events:handleOriginal(source, event, ...)
+        local callback = self.originalListeners[event]
+
+        if callback then
+            callback(...)
+        end
     end
 -- end of Events
 
--- tests/Facades/EventsTest.lua
+++ tests/Facades/EventsTest.lua
@@ -1,8 +1,27 @@
 TestEvents = {}
+    -- @covers Events:handleOriginal
+    function TestEvents:testHandleOriginal()
+        local events = __:new('Events')
+
+        local callbackInvoked = false
+        local callback = function (param) callbackInvoked = param end
+
+        events:handleOriginal(nil, 'test-event', 'test-arg')
+
+        lu.assertIsFalse(callbackInvoked)
+
+        events.originalListeners['test-event'] = callback
+
+        events:handleOriginal(nil, 'test-event', 'test-arg')
+
+        lu.assertEquals('test-arg', callbackInvoked)
+    end
+
     -- @covers StormwindLibrary.events
     function TestEvents:testLibraryInstanceIsSet()
         local events = __.events
 
         lu.assertNotIsNil(events)
+        lu.assertIsTable(events.originalListeners)
     end
 -- end of TestEvents
