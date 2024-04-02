e7b59ccc779bdb717526606ce10859263f76309d
Adriano Castro <adrianostanley@gmail.com>
2024-04-02 10:39:26 -0300

    Add documentation about the Bool:isFalse() method

--- src/Support/Bool.lua
+++ src/Support/Bool.lua
@@ -12,6 +12,17 @@ local Bool = {}
     This method checks if a value is in a range of possible values that
     represent a true value.
 
+    @NOTE: Developers may notice this class has no isFalse() method.
+           In terms of determining if a value is true, there's a limited
+           range of values that can be considered true. On the other hand,
+           anything else shouldn't be considered false. Consumers of this
+           class can use isTrue() to determine if a value represents a true
+           value, but using a isFalse() method would be a bit inconsistent.
+           That said, instead of having a isFalse() method, consumers can
+           use the not operator to determine if a value is false, which
+           makes the code more readable, like: if this value is not true,
+           then do something.
+
     @tparam mixed value
 
     @treturn bool
