bcd9dfcbd3ad1052de9f0d77b1730c8a14cbf125
Adriano Castro <adrianostanley@gmail.com>
2024-05-21 08:03:33 -0300

    Config docusaurus to deploy to GitHub pages

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://adrianocastro189.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/stormwind-library/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'adrianocastro189', // Usually your GitHub org/user name.
+  projectName: 'stormwind-library', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
