da5b85711bda65861c29ec0b6547e8dd33163bb8
Adriano Castro <adrianostanley@gmail.com>
2024-05-21 14:57:59 -0300

    Final tweaks to set www.stormwindlibrary.com up

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://adrianocastro189.github.io',
+  url: 'https://www.stormwindlibrary.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/stormwind-library/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
