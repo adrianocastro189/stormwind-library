e4f0797b43fbaa83c6410e6c4f968690ad752c04
Adriano Castro <adrianostanley@gmail.com>
2024-06-06 08:53:32 -0300

    Add the ItemFactory:createFromContainerItemInfo() method

--- tests/Factories/ItemFactoryTest.lua
+++ tests/Factories/ItemFactoryTest.lua
@@ -3,4 +3,28 @@ TestItemFactory = BaseTestClass:new()
     function TestItemFactory:testLibraryInstanceIsSet()
         lu.assertNotNil(__.itemFactory)
     end
+
+    -- @covers ItemFactory:createFromContainerItemInfo()
+    function TestItemFactory:testCreateFromContainerItemInfo()
+        local function execution(containerItemInfo, expectedResult)
+            local item = __.itemFactory:createFromContainerItemInfo(containerItemInfo)
+
+            if not expectedResult then
+                lu.assertIsNil(item)
+                return
+            end
+
+            lu.assertEquals(expectedResult.name, item.name)
+            lu.assertEquals(expectedResult.id, item.id)
+        end
+
+        execution(nil, nil)
+        execution({}, __:new('Item'))
+        execution({
+            ['itemName'] = 'test-item-name',
+            ['itemID'] = 1
+            }, __:new('Item')
+                :setName('test-item-name')
+                :setId(1))
+    end
 -- end of TestItemFactory
