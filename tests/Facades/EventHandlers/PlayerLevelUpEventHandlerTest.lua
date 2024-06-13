0c4c31d37170b6bc1ddb97446902a040a38e4148
Adriano Castro <adrianostanley@gmail.com>
2024-06-13 15:35:38 -0300

    Listen to the PLAYER_LEVEL_UP event

--- tests/Facades/EventHandlers/PlayerLevelUpEventHandlerTest.lua
+++ tests/Facades/EventHandlers/PlayerLevelUpEventHandlerTest.lua
@@ -1,2 +1,11 @@
 TestPlayerLevelUpEventHandler = BaseTestClass:new()
+    -- @covers PlayerLevelUpEventHandler.lua
+    function TestPlayerLevelUpEventHandler:testEventNameIsSet()
+        lu.assertEquals('PLAYER_LEVEL_UP', __.events.EVENT_NAME_PLAYER_LEVEL_UP)
+    end
+
+    -- @covers PlayerLevelUpEventHandler.lua
+    function TestPlayerLevelUpEventHandler:testPlayerLevelUpIsBeingWatched()
+        lu.assertIsFunction(__.events.originalListeners['PLAYER_LEVEL_UP'])
+    end
 -- end of TestPlayerLevelUpEventHandler
