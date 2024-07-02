fe79948f083a3fe0b56a2ba3b0fc3c79acda2df6
Adriano Castro <adrianostanley@gmail.com>
2024-07-02 11:09:17 -0300

    Add the PlayerCombatStatusEventHandler.lua file

--- src/stormwind-library.lua
+++ src/stormwind-library.lua
@@ -18,6 +18,7 @@
 -- import src/Commands/CommandsHandler.lua
 
 -- import src/Facades/Events.lua
+-- import src/Facades/EventHandlers/PlayerCombatStatusEventHandler.lua
 -- import src/Facades/EventHandlers/PlayerLevelUpEventHandler.lua
 -- import src/Facades/EventHandlers/PlayerLoginEventHandler.lua
 -- import src/Facades/EventHandlers/TargetEventHandler.lua
