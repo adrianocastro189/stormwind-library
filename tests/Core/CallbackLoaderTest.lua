349a30a4ac3a511cd83bb6a54476dc899e3847cb
Adriano Castro <adrianostanley@gmail.com>
2024-07-16 12:18:16 -0300

    Implement the callback loader mechanism

--- tests/Core/CallbackLoaderTest.lua
+++ tests/Core/CallbackLoaderTest.lua
@@ -1,2 +1,11 @@
 TestCallbackLoader = BaseTestClass:new()
+    -- @covers CallbackLoader.lua
+    function TestCallbackLoader:testOnLoad()
+        lu.assertIsTrue(__.callbacksInvoked)
+
+        lu.assertIsNil(__.destroyCallbackLoader)
+        lu.assertIsNil(__.invokeLoadCallbacks)
+        lu.assertIsNil(__.loadCallbacks)
+        lu.assertIsNil(__.onLoad)
+    end
 -- end of TestCallbackLoader
