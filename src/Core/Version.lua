94578bb355ee2c0bbb5f9be81e47fd13009d4579
Adriano Castro <adrianostanley@gmail.com>
2024-07-29 13:53:48 -0300

    Add the library getVersionedNameLabel() method

--- src/Core/Version.lua
+++ src/Core/Version.lua
@@ -1,4 +1,20 @@
 --[[--
+Gets a formatted versioned name label for the addon.
+
+This method is similar to getVersionLabel(), but it also includes the addon
+name in the label.
+
+When the version is not set, it will return the addon name only.
+
+@treturn string The addon name and version label
+]]
+function self:getVersionedNameLabel()
+    local versionLabel = self:getVersionLabel()
+
+    return self.addon.name .. (versionLabel and ' ' .. versionLabel or '')
+end
+
+--[[--
 Gets a formatted version label for the addon.
 
 By default, a version label is simply the version number prefixed with a 'v'.
--- tests/Core/VersionTest.lua
+++ tests/Core/VersionTest.lua
@@ -1,4 +1,21 @@
 TestVersion = BaseTestClass:new()
+    -- @covers StormwindLibrary:getVersionedNameLabel()
+    function TestVersion:testGetVersionedNameLabel()
+        local function execution(version, expectedOutput)
+            __.addon.version = version
+
+            lu.assertEquals(expectedOutput, __:getVersionedNameLabel())
+        end
+
+        __.addon.name = 'TestAddon'
+
+        -- version is set
+        execution('1.11.0', 'TestAddon v1.11.0')
+
+        -- version is not set
+        execution(nil, 'TestAddon')
+    end
+
     -- @covers StormwindLibrary:getVersionLabel()
     function TestVersion:testGetVersionLabel()
         local function execution(version, expectedOutput)
