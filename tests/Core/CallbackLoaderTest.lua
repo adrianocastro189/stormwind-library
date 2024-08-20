926b92884dc2cf348216f0fe0b6d21e54711779e
Adriano Castro <adrianostanley@gmail.com>
2024-08-20 13:44:26 -0300

    Update TestCallbackLoader to the new TestCase structure

--- tests/Core/CallbackLoaderTest.lua
+++ tests/Core/CallbackLoaderTest.lua
@@ -1,13 +1,15 @@
--- @TODO: Move this test class to the new TestCase structure <2024.07.30>
-
 TestCallbackLoader = BaseTestClass:new()
-    -- @covers CallbackLoader.lua
-    function TestCallbackLoader:testOnLoad()
+
+TestCase.new()
+    :setName('onLoad')
+    :setTestClass(TestCallbackLoader)
+    :setExecution(function()
         lu.assertIsTrue(__.callbacksInvoked)
 
         lu.assertIsNil(__.destroyCallbackLoader)
         lu.assertIsNil(__.invokeLoadCallbacks)
         lu.assertIsNil(__.loadCallbacks)
         lu.assertIsNil(__.onLoad)
-    end
+    end)
+    :register()
 -- end of TestCallbackLoader
